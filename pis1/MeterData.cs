b8a6aecadd2e629f0548459b011f7a42a9033fc2
Ezhok <fedina2005@inbox.ru>
2024-09-29 14:19:22 +0700

    Добавьте файлы проекта.

---
+++ pis1/MeterData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class Meteo
+    {
+        public string place { get; set; }
+        public DateTime data {  get; set; }
+        public double value { get; set; }
+
+        public Meteo(string input)
+        {
+            input = input.Replace("'", "");
+            string[] dates = input.Split(';');
+
+            place = dates[0];
+            data = DateTime.ParseExact(dates[1], "yyyy.MM.dd", CultureInfo.InvariantCulture);
+            
+            value = double.Parse(dates[2], CultureInfo.InvariantCulture);
+        
+        }
+    }
+}
---
+++ pis1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Meteo meterReading = new Meteo("'Санкт-Пиетербург';2005.03.27;24.6");
+
+            Console.WriteLine($"Место измерения: {meterReading.place}");
+            Console.WriteLine($"Дата измерений:{meterReading.data.ToString("yyyy.MM.dd")}") ;
+            Console.WriteLine($"Результаты измерений: {meterReading.value}");
+            Console.WriteLine("T,encz rjirb");
+        }
+    }
+}
